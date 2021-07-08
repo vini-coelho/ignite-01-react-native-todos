2f73920bd3f070428e7635897f1a3dde8b3f3d83
Vinicius Coelho <vinicius_coelho@live.com>
2021-07-08 12:13:49 -0400

    finalizando desafio 01

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,15 +9,27 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    const newTask = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false
+    };
+    
+    setTasks([...tasks, newTask]);
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+    const updatedTasks = tasks.map(task => 
+      task.id === id ? {...task, done: !task.done} : task 
+    );
+
+    setTasks(updatedTasks);
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    const filteredTasks = tasks.filter(task => task.id !== id);
+
+    setTasks(filteredTasks);
   }
 
   return (
